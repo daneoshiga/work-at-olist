4923abd470b57a10d37b476c151f57bf9efaa4cd
Danilo Shiga <daniloshiga@gmail.com>
2016-07-29 19:29:26 -0300

    Adding basic model structure to core app

--- work-at-olist/core/models.py
+++ work-at-olist/core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from mptt.models import MPTTModel, TreeForeignKey
+
+
+class Channel(models.Model):
+    name = models.CharField('Name', max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(MPTTModel):
+    name = models.CharField('Name', max_length=255)
+    parent = TreeForeignKey('self', null=True, blank=True,
+                            related_name='children', db_index=True)
+    channel = models.ForeignKey(Channel)
+
+    def __str__(self):
+        return self.name
