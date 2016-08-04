30a8ecae83cde04b8f70fda11ac54adaf27eaf56
Danilo Shiga <daniloshiga@gmail.com>
2016-08-04 11:07:21 -0300

    Names are required

--- work-at-olist/core/models.py
+++ work-at-olist/core/models.py
@@ -5,7 +5,7 @@ from mptt.models import MPTTModel, TreeForeignKey
 
 class Channel(models.Model):
     """Channels (eg. marketplaces)"""
-    name = models.CharField('Name', max_length=255)
+    name = models.CharField('Name', max_length=255, blank=False)
 
     def __str__(self):
         return self.name
@@ -13,7 +13,7 @@ class Channel(models.Model):
 
 class Category(MPTTModel):
     """Categories Tree Structure of each channel"""
-    name = models.CharField('Name', max_length=255)
+    name = models.CharField('Name', max_length=255, blank=False)
     parent = TreeForeignKey('self', null=True, blank=True,
                             related_name='children', db_index=True)
     channel = models.ForeignKey(Channel)
