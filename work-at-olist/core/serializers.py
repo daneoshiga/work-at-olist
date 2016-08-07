2e1d5b1cb6f22811d6c4b29c660303a1ed428089
Danilo Shiga <daniloshiga@gmail.com>
2016-08-07 00:29:59 -0300

    Adding views and serializers for the next two endpoints

--- work-at-olist/core/serializers.py
+++ work-at-olist/core/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from core.models import Channel
+from core.models import Channel, Category
 
 
 class ChannelSerializer(serializers.ModelSerializer):
@@ -9,3 +9,20 @@ class ChannelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Channel
         fields = ('channel',)
+
+
+class CategoryListSerializer(serializers.ModelSerializer):
+    category = serializers.CharField(source='name')
+    children = serializers.StringRelatedField(many=True)
+
+    class Meta:
+        model = Category
+        fields = ('category', 'level', 'children')
+
+
+class CategoryFamilySerializer(serializers.ModelSerializer):
+    category = serializers.CharField(source='name')
+
+    class Meta:
+        model = Category
+        fields = ('category', 'level', 'id')
--- work-at-olist/core/views.py
+++ work-at-olist/core/views.py
@@ -1,8 +1,9 @@
 from rest_framework import mixins
 from rest_framework import generics
 
-from core.models import Channel
-from core.serializers import ChannelSerializer
+from core.models import Channel, Category
+from core.serializers import (ChannelSerializer, CategoryListSerializer,
+                              CategoryFamilySerializer)
 
 
 class ChannelList(mixins.ListModelMixin, generics.GenericAPIView):
@@ -11,3 +12,29 @@ class ChannelList(mixins.ListModelMixin, generics.GenericAPIView):
 
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
+
+
+class CategoryList(mixins.ListModelMixin, generics.GenericAPIView):
+    serializer_class = CategoryListSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def get_queryset(self):
+        channel = self.kwargs.get('channel')
+        return (Category.objects
+                .filter(channel=channel)
+                .filter(level=0))
+
+
+class CategoryFamily(mixins.ListModelMixin, generics.GenericAPIView):
+    serializer_class = CategoryFamilySerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def get_queryset(self):
+        pk = self.kwargs.get('category_id')
+        return (Category.objects
+                .get(pk=pk)
+                .get_family())
