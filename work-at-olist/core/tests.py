e263c23e7f5646d7824fff1c83e8e17fc9d62a12
Danilo Shiga <daniloshiga@gmail.com>
2016-08-08 14:00:45 -0300

    Refactoring tests to use reversed urls instead of hardcoded ones

--- work-at-olist/core/tests.py
+++ work-at-olist/core/tests.py
@@ -1,16 +1,20 @@
 import pytest
 import json
 
+from django.core.urlresolvers import reverse
+
 from core.models import Channel, Category
 
 
 @pytest.fixture
 def initial_data():
-    data = {}
-    data['channel'] = Channel.objects.create(name='testchannel')
-    data['category'] = Category.objects.create(channel=data['channel'],
-                                               name='Category 1')
-    return data
+    channel = Channel.objects.create(name='testchannel')
+    category = Category.objects.create(channel=channel,
+                                       name='Category 1')
+    category_2 = Category.objects.create(channel=category.channel,
+                                         parent=category, name='Category 2')
+
+    return {'channel': channel, 'category': category, 'category_2': category_2}
 
 
 @pytest.mark.django_db
@@ -25,15 +29,17 @@ class TestModel:
 @pytest.mark.django_db
 class TestAPI:
     def test_get_channels_is_up(self, client, initial_data):
-        response = client.get('/api/1/channels/')
+        url = reverse('core:channel_list')
+        response = client.get(url)
         assert response.status_code == 200
 
     def test_get_channels_content(self, client, initial_data):
+        url = reverse('core:channel_list')
         content = [{
             'id': str(initial_data['channel'].pk),
             'channel': 'testchannel'
         }]
-        response = client.get('/api/1/channels/')
+        response = client.get(url)
 
         assert b'testchannel' in response.content
         assert content == json.loads(response.content.decode())
@@ -43,31 +49,48 @@ class TestAPI:
         Category.create_channel_categories(channel='testchannel',
                                            categories=categories)
 
-        url = '/api/1/channel/{}/'.format(initial_data['channel'].pk)
+        url = reverse('core:category_list',
+                      kwargs={'channel': initial_data['channel'].pk})
+
         response = client.get(url)
         assert response.status_code == 200
 
     def test_get_channels_categories_content(self, client, initial_data):
-        url = '/api/1/channel/{}/'.format(initial_data['channel'].pk)
+        channel = initial_data['channel']
+        category = initial_data['category']
+        category_2 = initial_data['category_2']
+
+        url = reverse('core:category_list',
+                      kwargs={'channel': channel.pk})
+
         response = client.get(url)
-        assert b'Category 1' in response.content
+
+        content = [{'id': str(category.pk), 'category': category.name,
+                    'children': [{
+                        'id': str(category_2.pk), 'category': category_2.name,
+                        'children': []
+                    }]}]
+        assert content == json.loads(response.content.decode())
 
     def test_get_category_family_is_up(self, client, initial_data):
-        url = '/api/1/category/{}/'.format(initial_data['category'].pk)
+        category = initial_data['category']
+
+        url = reverse('core:category_family',
+                      kwargs={'category_id': category.pk})
+
         response = client.get(url)
         assert response.status_code == 200
 
     def test_get_category_family_content(self, client, initial_data):
         category = initial_data['category']
-        category_2 = Category.objects.create(channel=category.channel,
-                                             parent=category,
-                                             name='Category 2')
+        category_2 = initial_data['category_2']
         Category.objects.create(channel=category.channel, name='Other Tree')
 
         content = [{'id': str(category.pk), 'category': category.name},
                    {'id': str(category_2.pk), 'category': category_2.name}]
 
-        url = '/api/1/category/{}/'.format(category.pk)
+        url = reverse('core:category_family',
+                      kwargs={'category_id': category.pk})
         response = client.get(url)
         print(response.content)
         assert b'Category 2' in response.content
--- work-at-olist/core/urls.py
+++ work-at-olist/core/urls.py
@@ -3,8 +3,9 @@ from django.conf.urls import url
 from core import views
 
 urlpatterns = [
-    url(r'^channels/$', views.ChannelList.as_view()),
-    url(r'^channel/(?P<channel>[a-z0-9-]+)/$', views.CategoryList.as_view()),
+    url(r'^channels/$', views.ChannelList.as_view(), name='channel_list'),
+    url(r'^channel/(?P<channel>[a-z0-9-]+)/$', views.CategoryList.as_view(),
+        name='category_list'),
     url(r'^category/(?P<category_id>[a-z0-9-]+)/$',
-        views.CategoryFamily.as_view()),
+        views.CategoryFamily.as_view(), name='category_family'),
 ]
